f86650e6800f40f34161d86cc183885ded2a5706
breidamerkurjokull <alp.uluocak@outlook.com>
2023-10-15 14:50:18 +0200

    Change in the backend

--- app.py
+++ app.py
@@ -24,13 +24,14 @@ origins = [
     "http://localhost:3000",
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get("/")
