3b8a6076e31798e1478a1416d0833f3f443f6ba3
breidamerkurjokull <alp.uluocak@outlook.com>
2024-03-03 13:20:36 +0100

    Fix as http

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ app = FastAPI()
 
 origins = [
     "https://localhost:5174",
-    "https://localhost:5173",
-    "https://localhost:8000",
+    "http://localhost:5173",
+    "http://localhost:8000",
     "https://localhost:3000",
     "http://16.171.32.70:8000",
     "https://interview-bot-front-56ny04ckl-alp-uluocaks-projects.vercel.app",
